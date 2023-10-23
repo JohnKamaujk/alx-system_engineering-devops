b407d8f6cafd43a7a2ebc8d3b8df55d7f28fc08b
JohnKamaujk <johnjkkamau@gmail.com>
2023-10-23 23:34:12 +0300

    :sparkles:feat:get request

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,42 +1,58 @@
 #!/usr/bin/python3
-"""
-This script will fetch the TODO list for the employee with
-the provided ID and display the progress of each task.
-Make sure to have the requests module installed
-before running the script.
-"""
-
-import json
+"""Returns to-do list information for a given employee ID."""
 import requests
-from sys import argv
+import sys
+
+
+def get_employee_todo_progress(employee_id):
+    # Define the API endpoint
+    base_url = 'https://jsonplaceholder.typicode.com'
+    user_url = f'{base_url}/users/{employee_id}'
+    todos_url = f'{base_url}/todos'
+
+    # Fetch user data
+    user_response = requests.get(user_url)
+
+    if user_response.status_code != 200:
+        print(f"Error: Could not retrieve user data for ID {employee_id}")
+        return
+
+    user_data = user_response.json()
+    user_name = user_data.get('name')
+
+    if user_name is None:
+        print(f"Error: User name not found for ID {employee_id}")
+        return
+
+    # Fetch todos data for a specific user using the query parameter
+    params = {"userId": employee_id}
+    todos_response = requests.get(todos_url, params=params)
+
+    if todos_response.status_code != 200:
+        print(f"Error: Could not retrieve data for user ID {employee_id}")
+        return
+
+    todos_data = todos_response.json()
+
+    if not todos_data:
+        print(f"No TODO list data found for user ID {employee_id}")
+        return
+
+    total_tasks = len(todos_data)
+    completed_tasks = sum(1 for todo in todos_data if todo.get('completed'))
+
+    print(f"Employee {user_name} is done with tasks"
+          f"({completed_tasks}/{total_tasks}):")
+
+    for todo in todos_data:
+        if todo.get('completed'):
+            print(f"	 {todo.get('title')}")
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
 
-    sessionReq = requests.Session()
-
-    idEmp = argv[1]
-    idURL = (
-        'https://jsonplaceholder.typicode.com/users/{}/todos'
-        .format(idEmp)
-        )
-    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
-
-    employee = sessionReq.get(idURL)
-    employeeName = sessionReq.get(nameURL)
-
-    json_req = employee.json()
-    name = employeeName.json().get('name')
-
-    totalTasks = 0
-
-    for done_tasks in json_req:
-        if done_tasks['completed']:
-            totalTasks += 1
-
-    print("Employee {} is done with tasks({}/{}):".
-          format(name, totalTasks, len(json_req)))
-
-    for done_tasks in json_req:
-        if done_tasks['completed']:
-            print("\t " + done_tasks.get('title'))
+    employee_id = int(sys.argv[1])
+    get_employee_todo_progress(employee_id)
