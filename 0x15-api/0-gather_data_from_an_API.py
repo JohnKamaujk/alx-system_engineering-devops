b13aa9fbd2523992d67f4fa6f4682e9d2023c5d3
JohnKamaujk <johnjkkamau@gmail.com>
2023-10-23 22:19:29 +0300

    :sparkles:feat:export to json

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,24 +10,24 @@ import sys
 
 def user_data_from_api(userId):
     user = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(userId)
+        "https://jsonplaceholder.typicode.com/users/{}".format(userId)
         )
-    name = user.json().get('name')
+    name = user.json().get("name")
     response = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(userId)
+        "https://jsonplaceholder.typicode.com/users/{}/todos".format(userId)
         )
     data = response.json()
 
     total_task = len(data)
     completed_task = 0
     for task in data:
-        if task.get('completed') is True:
+        if task.get("completed") is True:
             completed_task += 1
-    print('Employee {} is done with tasks({}/{}):'
+    print("Employee {} is done with tasks({}/{}):"
           .format(name, completed_task, total_task))
     for task in data:
-        if task.get('completed') is True:
-            print("\t {}".format(task.get('title')))
+        if task.get("completed") is True:
+            print("\t {}".format(task.get("title")))
 
 
 if __name__ == "__main__":
