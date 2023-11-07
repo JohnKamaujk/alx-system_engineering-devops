2c49aebbcec5b26c3926821215f459a6f6b85430
JohnKamaujk <johnjkkamau@gmail.com>
2023-11-07 12:47:56 +0300

    :sparkles:feat:count occurence of keywords

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -34,9 +34,8 @@ def count_words(subreddit, word_list, after=None):
         after = data.get('data', {}).get('after')
         if after:
             return count_words(subreddit, word_list, after, counts)
-        
+
         if len(counts) == 0:
-            print("")
             return
 
         # If no more pages, print the sorted results
@@ -44,5 +43,4 @@ def count_words(subreddit, word_list, after=None):
         for word, count in sorted_counts:
             print("{}: {}".format(word, count))
     else:
-        print("")
         return
