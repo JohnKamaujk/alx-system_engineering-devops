a498b3cc56ef61f4deb8aba579496f3476d1e915
JohnKamaujk <johnjkkamau@gmail.com>
2023-11-07 13:49:26 +0300

    :sparkles:feat:count occurence of keywords

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -47,8 +47,6 @@ def count_words(subreddit, word_list, after=None, counts={}):
             print("{}: {}".format(word, count))
 
     elif response.status_code == 404:
-        print("")
         return
     else:
-        print("")
         return
